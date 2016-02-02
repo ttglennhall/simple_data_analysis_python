41daf60ae0c5602b76f3b6061bc394cb2ed3d65a
Tiffany Glenn-Hall <ttglennhall@gmail.com>
2016-02-01 21:11:42 -0800

    Graphs are side by side to see a comparison of amount requested vs amount funded A

--- lending_club_univariate_analysis.py
+++ lending_club_univariate_analysis.py
@@ -7,21 +7,26 @@ loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loan
 loansData.dropna(inplace=True) #drop NA values
 
 #boxplot
-loansData.boxplot(column=['Amount.Requested','Amount.Funded.By.Investors'])
+loansData.boxplot(column=['Amount.Requested','Amount.Funded.By.Investors'], return_type='dict')
 plt.title('Amount Requested vs Amount Funded')
-plt.show()
+#plt.show()
+plt.savefig("boxplot_requested_vs_funded.png")
 
 #histogram
 lab = ['Amount Requested', 'Amount Funded']
 plt.hist([loansData['Amount.Requested'],loansData['Amount.Funded.By.Investors']], stacked=True, label=lab)
 plt.legend(loc='upper right')
 plt.title('Amount Requested vs Amount Funded')
-plt.show()
+#plt.show()
+plt.savefig("hist_requested_vs_funded.png")
 
-ax1 = subplot(211)
+#qq plot
+ax1 = plt.subplot(211)
 graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
 
-ax2 = subplot(212)
-plt.figure()
+ax2 = plt.subplot(212)
 graph = stats.probplot(loansData['Amount.Funded.By.Investors'], dist="norm", plot=plt)
-plt.show()
+#plt.show()
+plt.savefig("qqplot_requested_vs_funded.png")
+
+
